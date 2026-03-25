fcbe60d4184b54feae889ffd57430f506dc5de52
Dibakar <dibakarroy.co.in@gmail.com>
2026-03-26 01:12:01 +0530

    simple commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path= require('path');
+
+const {connectToMongoDB} = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+const staticRoute = require('./routes/staticRouter');
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB('mongodb://localhost:27017/shorter-url')
+    .then(() => console.log('Mongodb connected')
+);
+
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/url", urlRoute);
+app.use("/",staticRoute);
+
+app.get('/url/:shortId', async(req, res) => {
+    const entry = await URL.findOneAndUpdate({
+        shortId: req.params.shortId
+    }, { 
+            $push: {
+                visitHistory: {
+                  timestamp: Date.now(),
+                },
+            }
+        }
+    );
+    if(!entry){
+        return res.status(404).send("URL not found");
+    }
+    res.redirect(entry.redirectURL);
+})
+
+app.listen(PORT, () => console.log(`Server Started at PORT :${PORT}`))
