7410edf1b1de6b4fca6485a3d82729e355167b70
Dibakar <dibakarroy.co.in@gmail.com>
2026-03-27 11:17:49 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const path= require('path');
+const path= require('path'); 
+const cookieParser = require('cookie-parser');
 
 const {connectToMongoDB} = require("./connect");
+const {restrictToLoggedinUserOnly , checkAuth} = require('./middleware/auth');
 const urlRoute = require("./routes/url");
+
 const URL = require("./models/url");
 const staticRoute = require('./routes/staticRouter');
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -18,9 +22,12 @@ app.set('views', path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
-app.use("/",staticRoute);
+
+app.use("/url", restrictToLoggedinUserOnly , urlRoute);
+app.use("/",checkAuth ,staticRoute);
+app.use("/user", userRoute)
 
 app.get('/url/:shortId', async(req, res) => {
     const entry = await URL.findOneAndUpdate({
